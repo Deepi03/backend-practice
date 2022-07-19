4ac9e2f56084866e6f9156460e309e61633b59a7
Deepika Malini Rajasekar <deepirajsekar03@gmail.com>
2022-07-19 16:53:39 +0300

    add mongoose db server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
+import mongoose from "mongoose";
+
 import app from "./app";
 
-app.listen(app.get("port"), () =>
-  console.log(`app is up and running in port ${app.get("port")}`)
-);
+/* mongoose
+  .connect(
+    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.1"
+  )
+  .then(() => {
+    app.listen(app.get("port"), () =>
+      console.log(`app is up and running in port ${app.get("port")}`)
+    );
+  })
+  .catch(e => {
+    console.log("mongodb connect error");
+    process.exit(1);
+  }); */
+mongoose
+  .connect(
+    "mongodb+srv://deepirajsekar03:1234@cluster0.3cqlze3.mongodb.net/backend-practice?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(app.get("port"), () =>
+      console.log(`app is up and running in port ${app.get("port")}`)
+    );
+  })
+  .catch(e => {
+    console.log("mongodb connect error");
+    process.exit(1);
+  });
---
+++ src/models/Authors.ts
@@ -0,0 +1,6 @@
+import { Schema } from "mongoose";
+
+const authorSchema = new Schema({
+  fullname: String,
+  avatar: String
+});
